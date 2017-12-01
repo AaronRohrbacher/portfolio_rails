55232e65ec4b98becbec4c5fb413c81015400414
Aaron Rohrbacher <arjazz@icloud.com>
2017-12-01 11:46:22 -0800

    customize user data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-protected
+  protected
 
-def configure_permitted_parameters
-added_attrs = [:username, :email, :first_name, :last_name, :password, :password_confirmation, :remember_me]
-devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-end
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :first_name, :last_name, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 
 end
--- db/migrate/20171201193156_devise_create_users.rb
+++ db/migrate/20171201193156_devise_create_users.rb
@@ -2,8 +2,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :username,           null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+
+      ##Custom saved data:
+      t.string :email,              null: false, default: ""
+      t.string :first_name,         null: false, default: ""
+      t.string :last_name,          null: false, default: ""
+      
 
       ## Recoverable
       t.string   :reset_password_token
