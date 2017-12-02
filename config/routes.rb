1df8e6d6fbea7051749ff01db20ca2c1c4813f7f
Aaron Rohrbacher <arjazz@icloud.com>
2017-12-01 16:02:14 -0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users do
-    resources :portfolios
-    resources :projects
-    resources :skills
+    resources :portfolios do
+      resources :projects
+      resources :skills
+    end
   end
 
 end
