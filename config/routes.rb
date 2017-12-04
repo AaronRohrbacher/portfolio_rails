072f755d961e8a44f7474aa5c13dfbc567cc845d
Aaron Rohrbacher <arjazz@icloud.com>
2017-12-03 18:08:09 -0800

    fix login/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root :to => 'home#index'
 
+
   devise_for :users
 
   resources :users do
@@ -11,4 +12,5 @@ Rails.application.routes.draw do
     resources :projects
     resources :skills
   end
+
 end
