ee995413aea7df3fd0141fa1ad3d203c1b7edf00
Aaron Rohrbacher <arjazz@icloud.com>
2017-12-03 16:50:40 -0800

    set up skills controller for new relationships

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users do
-    resources :portfolios do
-      resources :projects do
-        resources :skills
-      end
-    end
+    resources :portfolios
+    resources :projects
+    resources :skills
   end
-
 end
